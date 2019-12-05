b1604ae07b822acc8890da7c3525e809b20fb032
Joao Carlos Konewalik de Souza <jckonewalik@gmail.com>
2019-12-05 15:19:38 -0300

    create user redux structure

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import classReducer from './class/class.reducer';
 import attendanceReducer from './attendance/attendance.reducer';
+import userReducer from './user/user.reducer';
 
 const rootReducer = combineReducers({
   classes: classReducer,
   attendance: attendanceReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
